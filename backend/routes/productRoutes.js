47904baa906f1496988ceeb071f98db9b3953191
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-06 02:13:19 +0530

    fixed typo error

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const upoload = require('../middleware/multer');
 const productController = require('../controllers/productController');
 
 
