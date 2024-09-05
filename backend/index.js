d8bd86778c780376fc8e2f5bc4a65c3c7b7bdf6c
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-05 23:19:54 +0530

    fixed server issue

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,27 @@
-const express =  requrie('express');
+
+
+const express = require('express');
 const connnectDB = require('./config/db');
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
+const app = express(); // Create an Express app instance
 
-const app = express();
+// Middlewares
+app.use(express.json()); // Use JSON parsing middleware
+app.use(cors()); // Use CORS middleware
 
-// coneccitng to DB
+// Connecting to DB
 connnectDB();
 
-// middlewares
-app.use('/api/products',productRoutes);
-app.use('api/users',userRoutes);
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT , () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
