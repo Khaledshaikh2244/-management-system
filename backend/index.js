e7e8957cd8f10cfbad2c5fbaba5dcca9e1731f93
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-05 18:53:02 +0530

    server entry point

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,24 @@
 const express =  requrie('express');
 const connnectDB = require('./config/db');
 const cors = require('cors');
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+
+const app = express();
+
+// coneccitng to DB
+connnectDB();
+
+// middlewares
+app.use('/api/products',productRoutes);
+app.use('api/users',userRoutes);
+
+const PORT =  process.env.PORT || 5000;
+
+
+app.listen(PORT , () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
