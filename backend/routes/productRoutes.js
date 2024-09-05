81b082e139c4781f44ea6095af49ade9c087faaa
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-05 18:32:40 +0530

    added the products routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,4 +6,7 @@ const productController = require('../controllers/productController');
 
 // products endpointes
 router.get('/' , productController.getAllProducts);
-router.post('/',productController.)
+router.post('/',productController.addProduct);
+router.put('/:id',productController.updateProduct);
+
+module.exports = router;
