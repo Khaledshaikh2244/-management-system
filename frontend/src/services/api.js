04055cc836e4aba5b7395f5fe1eb82a2c154d845
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-06 04:13:48 +0530

    added api's

---
+++ frontend/src/services/api.js
@@ -0,0 +1,16 @@
+import axios from ('axios');
+
+const API_URL = 'http://local:5000/api';
+
+
+// here we will have product  api
+export const getProducts = () => axios.get(`${API_URL}/products`); 
+export const addProduct = (product) => axios.post(`${API_URL}/product`,product);
+export const updateProduct = (id, product) => axios.put(`${API_URL}/product/${id}`,product);
+
+
+
+// no w here users api
+export const getUsers = () => axios.get(`${API_URL}/usres`);
+export const addUser = (user) => axios.post(`${API_URL}/users`,user);
+export const deleteUser = (id) => axios.delete(`${API_URL}/users/${id}`);
