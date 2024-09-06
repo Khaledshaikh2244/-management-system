e8e444084972d87f81ba38e59aaa222ed432ff1a
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-06 12:11:01 +0530

    added component productList

---
+++ frontend/src/components/AdminPanel/ProductList.jsx
@@ -0,0 +1,42 @@
+import React,{useState, useEffect} from 'react';
+import {getProducs} from '../../services/api';
+
+
+
+
+ const ProductList = () => {
+
+  const [products ,setProducts] = useState([]);
+//   gettin our products from backend api;
+useEffect(() => {
+    const fetchProducts = async () =>{
+        try {
+            const response = await getProducs();
+            setProducts(response.data);
+        } catch (error) {
+            console.log('Eror fetching products', error);
+        };
+      }
+
+   fetchProducts(); 
+}, [])
+
+  return (
+    <div className='p-4'>
+    <h2 className='text-xl font-bold'>Product List</h2>
+    <ul className='list-item pl-5'>  
+    {products.map(product => (
+        <li key= {product._id} className='mb-4'>
+            <h3 className='text-lg semi-bold'>{product.productName}</h3>
+            <p>{product.description}</p>
+            <p>Price :${product.price} </p>
+            <p>Stock Quantity: {product.stockQuantity}</p>
+            <p>category :{product.category}</p>
+            {product.image && <img src={product.image} alt ={product.productName} className="mt-2 w-32 h-32 object-cover" />}
+        </li>
+    ))}
+    </ul>
+    </div>
+  );
+}
+export default ProductList;
