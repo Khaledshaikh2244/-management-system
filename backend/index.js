a1288c72c642fe0f23f3a3cc822fb23803a57be9
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-07 19:24:44 +0530

    fixed some bugs

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const connnectDB = require('./config/db');
 const cors = require('cors');
@@ -7,11 +5,11 @@ const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
-const app = express(); // Create an Express app instance
+const app = express(); 
 
 // Middlewares
-app.use(express.json()); // Use JSON parsing middleware
-app.use(cors()); // Use CORS middleware
+app.use(express.json()); 
+app.use(cors()); 
 
 // Connecting to DB
 connnectDB();
@@ -19,6 +17,16 @@ connnectDB();
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+
+// Added a route for the root URL
+app.get("/", (req, res) => {
+  res.send("Server is Running ! You can use these routes: http://localhost:5000/api/users and http://localhost:5000/api/products");
+});
+
+// Moved the middleware to a different route
+app.get('/status', (req, res) => {
+  res.send("Server is Running ! You can use these routes: http://localhost:5000/api/users and http://localhost:5000/api/products");
+});
 
 const PORT = process.env.PORT || 5000;
 
